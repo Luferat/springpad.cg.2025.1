65ccef7137fae0591a0aebf05e788c98034ec919
André Luferat <contato@luferat.net>
2025-10-23 09:29:31 -0300

    ok

---
+++ src/main/java/com/projetos/springpad/controller/owner/LoginController.java
@@ -0,0 +1,17 @@
+package com.projetos.springpad.controller.owner;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class LoginController {
+
+    @GetMapping("/login")
+    public String login(Model model) {
+
+        // Toda a lógica desta página
+
+        return "owner/login";
+    }
+}
